6392bb96294947362934a7fd7f7028b8cfdd20b5
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-07-13 23:37:46 +0300

    work branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import Impl.IntegerListImpl;
+
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
+
+
+
+        IntegerListImpl testList = new IntegerListImpl(7);
+
+        testList.add(0,1);
+        testList.add(1,2);
+        testList.add(2,3);
+        testList.add(3,4);
+        testList.add(4,5);
+
+        testList.remove(0);
+        System.out.println(testList.get(0));
+        testList.set(0,23);
+        System.out.println(testList.get(0));
+        System.out.println(testList.size());
+        System.out.println(testList.contains(234));
+        System.out.println(testList.indexOf(4));
+        System.out.println(Arrays.toString(testList.toArray()));
 
     }
 }
